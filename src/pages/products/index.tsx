bdf8cac28b48668f9b81afbdbd952fff2b56f44d
mahdi <mahdimotallebe@gmail.com>
2023-03-01 11:23:59 +0330

    Create user and product page

---
+++ src/pages/products/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+const Products = () => {
+  return (
+    <Box fontWeight={700} fontSize={30}>
+      Prodcuts Page
+    </Box>
+  );
+};
+
+export default Products;
---
+++ src/pages/user/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+const User = () => {
+  return (
+    <Box fontWeight={700} fontSize={30}>
+      User Page
+    </Box>
+  );
+};
+
+export default User;
