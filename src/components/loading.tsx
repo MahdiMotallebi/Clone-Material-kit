0726abc8f047402e7d970eb4e3caace640c6e3da
mahdi <mahdimotallebe@gmail.com>
2023-03-12 20:28:06 +0330

    Add loading in table user

---
+++ src/components/loading.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//mui
+import { Box } from '@mui/material';
+
+const Loading = () => {
+  return (
+    <Box className="loading">
+      <Box className="c1"></Box>
+      <Box className="c2"></Box>
+      <Box className="c3"></Box>
+      <Box className="c4"></Box>
+    </Box>
+  );
+};
+
+export default Loading;
--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -3,22 +3,39 @@ import axios from '../../utils/axiosinstance';
 
 //Mui
 import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
-import { Box, useTheme, Paper, MenuItem, Tooltip } from '@mui/material';
+import {
+  Box,
+  useTheme,
+  Paper,
+  MenuItem,
+  Tooltip,
+  Button,
+  Avatar,
+  Stack
+} from '@mui/material';
+import { useDemoData } from '@mui/x-data-grid-generator';
 import { tokens } from '../../theme/themeConfig';
 
 //context
 import { Post, useGlobalContext } from '../../context';
 
 //icons
-import { BsTrash, BsPencil } from 'react-icons/bs';
+import { BsTrash, BsPencil, BsPlus } from 'react-icons/bs';
 
 //components
 import UpdatePost from '../../components/modal';
+import Title from '../../components/chartTitle';
+import Loading from '../../components/loading';
 
 const User = () => {
   const { state, handleState } = useGlobalContext();
   const colors = tokens(state.mode);
   const theme = useTheme();
+  const { data } = useDemoData({
+    dataSet: 'Commodity',
+    rowLength: 100,
+    maxColumns: 6
+  });
 
   React.useEffect(() => {
     const fetchPosts = async () => {
@@ -55,20 +72,27 @@ const User = () => {
   const columns: GridColDef[] = [
     {
       field: '#',
-      headerName: '#',
-      width: 90,
+      headerName: 'Row',
+      width: 70,
       renderCell: (params: GridRenderCellParams<string>) => {
         return (
           <Box
             sx={{
-              display: 'flex',
-              alignItems: 'center',
-              width: '100%',
-              justifyContent: 'center'
+              width: '100%'
             }}
           >
             {params.id}
           </Box>
+        );
+      }
+    },
+    {
+      field: 'avatar',
+      headerName: 'Avatar',
+      width: 100,
+      renderCell: (params: GridRenderCellParams<string>) => {
+        return (
+          <Avatar alt="Remy Sharp" src="/assets/images/avatars/avatar_3.jpg" />
         );
       }
     },
@@ -83,14 +107,14 @@ const User = () => {
     {
       field: 'body',
       headerName: 'Body',
-      width: 500,
+      width: 450,
       renderCell: (params: GridRenderCellParams<string>) => {
-        return <Box>{params.row.body.slice(0, 70).concat('...')}</Box>;
+        return <Box>{params.row.body.slice(0, 60).concat('...')}</Box>;
       }
     },
     {
       field: 'action',
-      headerName: '',
+      headerName: 'Actions',
       width: 200,
       renderCell: (params: GridRenderCellParams<string>) => {
         return (
@@ -119,65 +143,100 @@ const User = () => {
   ];
 
   return (
-    <Paper
-      elevation={0}
-      sx={{
-        height: '95vh',
-        position: 'relative',
-        border: `1px solid ${
-          theme.palette.mode === 'light'
-            ? theme.palette.border.main
-            : theme.palette.border.dark
-        }`,
-        background: `${theme.palette.background.default}`,
-        borderRadius: '1rem',
-        '& .MuiDataGrid-root': {
-          border: 'none',
-          '& .MuiDataGrid-cell': {
-            borderBottom: `1px solid ${
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '100%',
+          my: '2rem'
+        }}
+      >
+        <Box>
+          <Title title="user" />
+        </Box>
+        <Box>
+          <Button
+            variant="contained"
+            startIcon={<BsPlus />}
+            sx={{ textTransform: 'capitalize' }}
+          >
+            new user
+          </Button>
+        </Box>
+      </Box>
+      {state.posts.length === 0 ? (
+        <Loading />
+      ) : (
+        <Paper
+          elevation={0}
+          sx={{
+            position: 'relative',
+            border: `1px solid ${
               theme.palette.mode === 'light'
                 ? theme.palette.border.main
                 : theme.palette.border.dark
             }`,
-            '&:focus-within': {
-              outline: 'none'
-            }
-          },
-          '& .MuiDataGrid-columnHeaders': {
-            '&:focus-within': {
-              outline: 'none'
-            }
-          }
-        },
+            background: `${theme.palette.background.paper}`,
+            borderRadius: '1rem',
+            '& .MuiDataGrid-root': {
+              border: 'none',
+              '& .MuiDataGrid-cell': {
+                borderBottom: `1px solid ${
+                  theme.palette.mode === 'light'
+                    ? theme.palette.border.main
+                    : theme.palette.border.dark
+                }`,
+                '&:focus-within': {
+                  outline: 'none'
+                }
+              },
+              '& .MuiDataGrid-columnHeaders': {
+                '&:focus-within': {
+                  outline: 'none'
+                }
+              }
+            },
 
-        '& .MuiDataGrid-columnHeaders': {
-          borderBottom: `1px solid ${
-            theme.palette.mode === 'light'
-              ? theme.palette.border.main
-              : theme.palette.border.dark
-          }`
-        },
-        '& .MuiDataGrid-virtualScroller': {
-          // backgroundColor: colors.primary[400]
-        },
-        '& .MuiDataGrid-footerContainer': {
-          borderTop: `1px solid ${
-            theme.palette.mode === 'light'
-              ? theme.palette.border.main
-              : theme.palette.border.dark
-          }`
-        },
-        '& .MuiCheckbox-root': {
-          //   color: `${colors.info[500]} `
-        }
-      }}
-    >
-      <DataGrid
-        disableSelectionOnClick
-        rows={state.posts !== undefined ? state.posts : []}
-        columns={columns}
-      />
-    </Paper>
+            '& .MuiDataGrid-columnHeaders': {
+              borderBottom: `1px solid ${
+                theme.palette.mode === 'light'
+                  ? theme.palette.border.main
+                  : theme.palette.border.dark
+              }`
+            },
+            '& .MuiDataGrid-virtualScroller': {
+              backgroundColor: `${
+                theme.palette.mode === 'light'
+                  ? colors.grey[900]
+                  : colors.primary[500]
+              }`
+            },
+            '& .MuiDataGrid-footerContainer': {
+              borderTop: `1px solid ${
+                theme.palette.mode === 'light'
+                  ? theme.palette.border.main
+                  : theme.palette.border.dark
+              }`
+            },
+            '& .MuiCheckbox-root': {
+              //   color: `${colors.info[500]} `
+            }
+          }}
+        >
+          <DataGrid
+            autoHeight
+            disableSelectionOnClick
+            rows={state.posts}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5, 10, 25]}
+            rowHeight={70}
+          />
+        </Paper>
+      )}
+    </>
   );
 };
 
