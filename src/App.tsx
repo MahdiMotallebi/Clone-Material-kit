317f6e35f70609ff1be615a6b5aea775a493ecec
mahdi <mahdimotallebe@gmail.com>
2023-03-18 00:21:25 +0330

    Fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-//style
-// import './styles/global.scss';
+
 //components
-import SidebarLeft from './pages/global/SidebarLeft';
 import ContextProvider from './context';
+import AppRoutes from './routes/indext';
 import GlobalStyle from './styles/globalStyle';
 
 const App: React.FC = () => {
   return (
     <ContextProvider>
       <GlobalStyle />
-      <SidebarLeft />
+      <AppRoutes />
     </ContextProvider>
   );
 };
